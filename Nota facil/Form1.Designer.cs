461501abc4cda3f0950cfac4bdf3ad36b2f83883
Mercena730 <matheus.santos730@outlook.com>
2019-12-23 11:14:58 -0300

    Inicio

---
+++ Nota facil/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Nota_facil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.lblcar = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(46, 625);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(356, 44);
+            this.progressBar.TabIndex = 0;
+            // 
+            // lblcar
+            // 
+            this.lblcar.AutoSize = true;
+            this.lblcar.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblcar.Location = new System.Drawing.Point(427, 626);
+            this.lblcar.Name = "lblcar";
+            this.lblcar.Size = new System.Drawing.Size(215, 43);
+            this.lblcar.TabIndex = 1;
+            this.lblcar.Text = "Carregando..";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(654, 692);
+            this.Controls.Add(this.lblcar);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label lblcar;
+    }
+}
+
